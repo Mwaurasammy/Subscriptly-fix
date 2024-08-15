936a5611184c429533763793f5e621a1f3998921
WAHU0508 <sharongikenye@gmail.com>
2024-08-15 20:57:16 +0300

    edits

---
+++ subscriptly/src/pages/NotificationsPage.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react'
+import {differenceInDays, addMonths, addWeeks} from 'date-fns'
+
+const NotificationsPage = ({user}) => {
+    const [notifications, setNotifications] = useState([])
+
+    useEffect(() => {
+        const fetchNotifications = () => {
+            if (user) {
+                fetch(`http://localhost:5000/users?name=${user}`)
+                .then(res => res.json())
+                .then(users => {
+                    if(user.length > 0) {
+                        const userData = users[0]
+                        const expiringSubscriptions = userData.subscriptions.filter(subscription => {
+                            const daysLeft = calculateDaysLeft(subscription.date_of_payment, subscription.billing_cycle)
+                            return daysLeft <=7
+                        })
+                        setNotifications(expiringSubscriptions)
+                    }
+                })
+                .catch(error => console.error('Error fetching subscriptions:', error))
+            }
+        }
+        fetchNotifications()
+    }, [user])
+
+    function calculateDaysLeft(dateOfPayment, billingCycle) {
+        const paymentDate = new Date(dateOfPayment)
+        let nextBillingDate;
+    
+        switch (billingCycle) {
+            case 'monthly':
+                nextBillingDate = addMonths(paymentDate, 1);
+                break;
+            case 'yearly':
+                nextBillingDate = addMonths(paymentDate, 12);
+                break;
+            case 'weekly':
+                nextBillingDate = addWeeks(paymentDate, 1);
+                break;
+            default:
+                return 'Invalid billing cycle'
+        }
+        const today = new Date();
+        return differenceInDays(nextBillingDate, today)
+    }
+
+    return (
+        <div>
+            <h2>Notifications</h2>
+            {notifications.length > 0 ? (
+                <ul>
+                    {notifications.map((subscription) => {
+                        const daysLeft = calculateDaysLeft(subscription.date_of_payment, subscription.billing_cycle)
+                        return (
+                        <li key={subscription.id}>
+                            Your subscription to {subscription.name} is expiring soon.
+                             ({daysLeft}) {daysLeft === 1 ? 'day' : 'days'} left!
+                        </li>
+                        )
+                    })}
+                </ul>
+            ) : (
+                <p>No subscriptions are expiring soon.</p>
+            )}
+        </div>
+    )
+}
+export default NotificationsPage
